4f303e2afbe7aff9b2037e90b8b8f8811db13f90
climbingirl <isvetlanaivanova@yandex.ru>
2025-04-23 21:25:58 +0500

    feat: imlement header and home page

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,28 @@
+import { NavLink } from 'react-router-dom';
+import { ROUTES } from '../../router/routes';
+import classNames from 'classnames';
 import styles from './header.module.scss';
 
+interface LinkClassProps {
+  isActive: boolean;
+}
+
 const Header = () => {
-  return <header className={styles.container}></header>;
+  const linkClass = ({ isActive }: LinkClassProps) =>
+    classNames({ [styles.nav_link]: true, [styles.nav_link_active]: isActive });
+
+  return (
+    <header className={styles.container}>
+      <nav className={styles.nav}>
+        <NavLink className={linkClass} to={ROUTES.REGISTRATION_BASIC}>
+          Basic registration
+        </NavLink>
+        <NavLink className={linkClass} to={ROUTES.REGISTRATION_HOOK_FORM}>
+          Hook Form registration
+        </NavLink>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,11 @@
+import styles from './home.module.scss';
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <section className={styles.container}>
+      <h1 className={styles.header}>Choose a convenient registration method above</h1>
+    </section>
+  );
 };
 
 export default Home;
