cf6754c57bf3886b6cd329f7f16f84fae9741a8d
climbingirl <isvetlanaivanova@yandex.ru>
2025-05-04 00:15:03 +0500

    refactor: delete forwardRef from ButtonSubmit

--- src/components/form/buttonSubmit/buttonSubmit.tsx
+++ src/components/form/buttonSubmit/buttonSubmit.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
+import React, { Ref } from 'react';
 import styles from './buttonSubmit.module.scss';
 
 interface ButtonSubmitProps {
   children: React.ReactNode;
   disabled: boolean;
+  ref: Ref<HTMLButtonElement>;
 }
 
-const ButtonSubmit = React.forwardRef<HTMLButtonElement, ButtonSubmitProps>(
-  ({ disabled, children }, ref) => {
-    return (
-      <button className={styles.item} type="submit" disabled={disabled} ref={ref}>
-        {children}
-      </button>
-    );
-  }
-);
+const ButtonSubmit = ({ disabled, children, ref }: ButtonSubmitProps) => {
+  return (
+    <button className={styles.item} type="submit" disabled={disabled} ref={ref}>
+      {children}
+    </button>
+  );
+};
 
 export default ButtonSubmit;
